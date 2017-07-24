4fc7c4a5212890a81e97e428a643cc2986c1c5af
Emmanuel Ndukwe <etechgist@gmail.com>
2017-07-24 20:33:05 +0100

    project clean up

--- js/myJs.js
+++ js/myJs.js
@@ -1,71 +1,83 @@
 // JavaScript Document
 $('document').ready(function(e) {
-	$('.btn-lg').mouseenter(function(){
-		$(this).fadeTo(100,0.4).fadeTo(1000,1);
-
-		});
-    $('.lux').hide();
-    $('.biz').hide();
-
-    $('#lm-lux').click(function(e){
-        $('.lux').fadeIn(1000, "swing");
+    $('.investment').hide();
+    $('.business').hide();
+    $('.lifestyle').hide();
+    $('#lifestyleButton').click(function(e){
+        $('.investment').hide();
+        $('.business').hide();
+        $('.lifestyle').fadeIn(1000, "swing");
         });
-
-    $('#lm-biz').click(function(e){
-        $('.biz').fadeIn(1000, "swing");
+    $('#investmentButton').click(function(e){
+        $('.business').hide();
+        $('.lifestyle').hide();
+        $('.investment').fadeIn(1000, "swing");
     });
-
-    $('.hidelux').click(function(e){
-       $('.lux').fadeOut(1000, "swing");
+    $('#businessButton').click(function(e){
+        $('.investment').hide();
+        $('.lifestyle').hide();
+       $('.business').fadeIn(1000, "swing");
     });
-
-    $('.hidebiz').click(function(e){
-        $('.biz').fadeOut(1000, "swing");
+    $('.closeup').click(function(e){
+        $('.investment').hide();
+        $('.business').hide();
+        $('.lifestyle').hide();
     });
-    var name = $('#name').val();
-    var email = $('#email').val();
-//    var subject = $('#subject').val();
-    var message = $('#message').val();
-
-    console.log(name);
-
-    $('#supportForm').hide();
-    $('#support').on('click', function(){
-        $('#supportForm').slideToggle(1000);
-    });
-
-
-
-
+    var feedback = '<div class="alert alert-success alert-dismissible fade show" role="alert">'+
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>'+
+        '</button>'+
+        '<strong>Thank you!</strong> Your feedback has been recieved.</div>';
+    var subscription = '<div class="alert alert-success alert-dismissible fade show" role="alert">'+
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>'+
+        '</button>'+
+        '<strong>Congratulations!</strong>You have been added to our mailing list</div>';
+    var errorMsg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">'+
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>'+
+        '</button>'+
+        '<strong>Sorry, </strong>something went wrong</div>';
     $("#myForm").on('submit', function(){
-        var name = $('#name').val();
-        var email = $('#email').val();
-//    var subject = $('#subject').val();
-        var message = $('#message').val();
-
-
-        console.log(name);
-        emailjs.send("gmail","template_NHzH2D6l",{
-            name: name,
-            notes: "Check this out!",
-            from_name: email,
-            to_name: "etechgist@gmail.com",
-            message_html: message,
-            reply_to: email
-        })
-            .then(function(response) {
-                console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-                $('#supportForm').slideUp('fast');
-                $("#myAlert").fadeIn(900);
-                $('#name').val('');
-                $('#email').val('');
-                $('#message').val('');
-            }, function(err) {
-                console.log("FAILED. error=", err);
-
-            });
+        if($('#email').val() !=='' && $('#message').val() !=='') {
+            var email = $('#email').val();
+            var message = $('#message').val();
+            emailjs.send("gmail", "template_NHzH2D6l", {
+                notes: "Check this out!",
+                from_name: email,
+                to_name: "etechgist@gmail.com",
+                message_html: message
+            })
+                .then(function (response) {
+                    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                    $('#email').val('');
+                    $('#message').val('');
+                    $('#content').html(feedback);
+                }, function (err) {
+                    console.log("FAILED. error=", err);
+                    $('#content').html(errorMsg);
+                });
+        }else{
+            $('#content').html(errorMsg);
+        }
         return false;
     });
+    $("#subForm").on('submit', function(){
+        if($('#email').val() !=='') {
+            var email = $('#emailList').val();
+            emailjs.send("gmail", "cash", {
+                email: email,
+                to_name: "etechgist@gmail.com"
+            })
+                .then(function (response) {
+                    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                    $('#emailList').val('');
+                    $('#content').html(subscription);
+                }, function (err) {
+                    console.log("FAILED. error=", err);
+                    $('#content').html(errorMsg);
 
-	
+                });
+        }else{
+            $('#content').html(errorMsg);
+        }
+        return false;
+    });
 });
